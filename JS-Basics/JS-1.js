1c38ec11fe47f59f22915161076b101fda322708
Anii8208 <154366261+Anii8208@users.noreply.github.com>
2023-12-20 05:52:30 +0000

    Learn Console,let,const,var,comment

--- JS-Basics/JS-1.js
+++ JS-Basics/JS-1.js
@@ -1,1 +1,27 @@
 console.log("Aniruddha")
+
+const UserId = 202020
+
+//var UserName = "Anii"
+//var is not recomended to use bcoz it create problem in scope of variable
+
+let UserName = "Anii"
+
+UserAdress = "New Bhim Nagar Akola"
+
+let UserCity;
+
+//UserId=808080
+/*console.log(UserId)
+console.log(UserName)
+console.log(UserAdress)
+*/
+
+UserCity = "Nagpur"
+
+let UserAccNo = 6530530
+
+console.log([UserId,UserName,UserAdress])
+
+console.log("User City="+UserCity)
+console.log("User Acc No=",UserAccNo)
