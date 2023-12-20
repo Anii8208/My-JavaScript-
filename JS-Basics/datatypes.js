75e48a080e007d9b4cc104755d5b3579ae2bc13e
Anii8208 <154366261+Anii8208@users.noreply.github.com>
2023-12-20 15:24:09 +0000

    datatypes

---
+++ JS-Basics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 4) //We are using nodejs not browser its have diff sintax for nodejs
+
+console.log(3
+    +
+    3); //code readability should be considerd
+
+// datatypes
+let name = "anii"
+let age = 28
+let  isLoggedIn = false
+
+/*
+//Primitive Datatypes
+
+    nubmber => 2 to power 53
+    bigint => large no use
+    string => ""
+    boolean => true/false
+    null => standalone value (object of class
+    undefined => currently value not assgin
+    symbol => identify Uniqe componants
+
+//Non-Primitive Datatypes
+
+        object
+
+ */
+
+//typeof
+        console.log(typeof undefined); // undefined
+        console.log(typeof null); //object
