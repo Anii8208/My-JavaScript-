8ef07917370ac9b061909962fe30c2b2ad4068f1
Anii8208 <154366261+Anii8208@users.noreply.github.com>
2023-12-21 18:39:29 +0000

    TypeConversion is done

--- JS-Basics/typeConversion.js
+++ JS-Basics/typeConversion.js
@@ -1,2 +1,46 @@
 let score = 33
-console.log(score);
+
+console.log(typeof score);
+
+//TypeConversion
+let scoreINnum = Number(score)
+
+console.log(typeof (scoreINnum));
+
+console.log(scoreINnum);
+
+/*
+       "33" => 33
+    "33abc" => NaN (Not an Number)
+    "anii"  => NaN
+      Null  => 0
+  Undefined => NaN
+      true  => 1 /  false => 0
+
+
+
+*/
+
+
+let IsLoggedIn = 1
+
+//TypeConversion 
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+
+console.log(typeof BooleanIsLoggedIn);
+
+console.log(BooleanIsLoggedIn);
+
+
+/*
+    1   =>  true
+    0   =>  false
+    "string"    =>  true
+    ""  =>  true
+    1545    =>  true
+    -1545   =>  true
+    -0  =>  false
+
+
+
+*/
