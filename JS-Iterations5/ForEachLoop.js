82785d7f031c15150448ffd6d350ac32be09b91a
Anii8208 <154366261+Anii8208@users.noreply.github.com>
2024-01-16 19:47:21 +0000

    ForEachLoop

---
+++ JS-Iterations5/ForEachLoop.js
@@ -0,0 +1,18 @@
+//For Each Loop = It is Use for Array
+
+const Myarr = ["JS", "CPP", "JAva", "HTML"]
+
+/*//Myarr.forEach( function (item) {
+    console.log(item);
+})*/
+
+/*Myarr.forEach( (Val) => {
+    console.log(Val);
+})*/
+
+/*function Printme(item) {
+  console.log(item);
+}
+
+Myarr.forEach(Printme)*/
+
