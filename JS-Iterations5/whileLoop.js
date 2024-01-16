097bc38d455a30dd56bb36330b5bc36f08ff87a4
Anii8208 <154366261+Anii8208@users.noreply.github.com>
2024-01-16 18:10:50 +0000

    while and do while loop

---
+++ JS-Iterations5/whileLoop.js
@@ -0,0 +1,30 @@
+//While Loop = it is also called entry control loop means the condtion check first at the initial position of loop
+
+/*let index = 0
+while (index<=10) {
+    //console.log(index);
+    index++
+}*/
+
+let MyArray = ["batman", "superman", "spiderman"]
+let i=0
+while (i<MyArray.length) {
+    //console.log(MyArray[i]);
+    i++
+}
+
+//*********************************************************************************** */
+
+//(Do while Loop) = it is also called as exit control loop means the condition is checked at the end of the loop 
+
+let index = 0
+/*do {
+    console.log(index);
+    index=index+1
+} while (index<=10);
+*/
+
+do {
+    console.log(index);
+    index=index+2
+} while (index<=20);
