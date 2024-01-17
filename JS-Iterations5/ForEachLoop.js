37c08b58cdfd0b50eb53692158f43c27a1767cdb
Anii8208 <154366261+Anii8208@users.noreply.github.com>
2024-01-17 09:32:33 +0000

    foreach LOOP

--- JS-Iterations5/ForEachLoop.js
+++ JS-Iterations5/ForEachLoop.js
@@ -12,7 +12,37 @@ const Myarr = ["JS", "CPP", "JAva", "HTML"]
 
 /*function Printme(item) {
   console.log(item);
-}
+}*/
 
-Myarr.forEach(Printme)*/
+//Myarr.forEach(Printme)
 
+//******************************************************************************* */
+
+/*Myarr.forEach((items, index, arr) => {
+    //console.log(`items = ${items}, index = ${index}, array = ${arr}`);
+})*/
+
+//*********************************************************************************** */
+
+//Objcets retrive in array
+
+const RecVal = [
+  {
+    LanguageName: "Java Script",
+    LanguageFileName: "Js"
+  },
+
+  {
+    LanguageName: "Python",
+    LanguageFileName : "Py"
+  },
+
+  {
+    LanguageName: "C++",
+    LanguageFileName : "cpp"
+  }
+]
+
+RecVal.forEach((Obj) => {
+    //console.log(Obj.LanguageFileName);
+})
